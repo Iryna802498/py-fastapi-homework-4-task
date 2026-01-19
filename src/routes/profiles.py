3a7765b827697f1db003a9a1be1a30d7ea079c1e
Iryna Mishchenko <djrobentab@gmail.com>
2026-01-19 14:19:46 +0100

    Solution

--- src/routes/profiles.py
+++ src/routes/profiles.py
@@ -1,5 +1,129 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Request, HTTPException, File, UploadFile
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from database import get_db, UserModel, UserProfileModel
+from security.http import get_token
+from security.interfaces import JWTAuthManagerInterface
+from storages.interfaces import S3StorageInterface
+from config import get_jwt_auth_manager, get_s3_storage_client
+from exceptions.security import TokenExpiredError, InvalidTokenError
+from validation.profile import validate_image
+from schemas.profiles import ProfileRequestSchema, ProfileResponseSchema
 
 router = APIRouter()
 
-# Write your code here
+
+async def validate_avatar(
+    avatar: UploadFile = File(...)
+) -> UploadFile:
+    result = validate_image(avatar=avatar)
+    return result
+
+
+async def get_current_user(
+    request: Request,
+    jwt_manager: JWTAuthManagerInterface = Depends(get_jwt_auth_manager),
+    db: AsyncSession = Depends(get_db)
+):
+    token = get_token(
+        request=request
+    )
+    try:
+        token_data = jwt_manager.decode_access_token(token=token)
+    except TokenExpiredError:
+        raise HTTPException(
+            status_code=401,
+            detail="Token has expired."
+        )
+    except InvalidTokenError:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid token."
+        )
+    user_id = token_data.get("user_id")
+    query = select(UserModel).where(
+        UserModel.id == user_id
+    )
+    result = await db.execute(query)
+    current_user = result.scalar_one_or_none()
+    if not current_user or not current_user.is_active:
+        raise HTTPException(
+            status_code=401,
+            detail="User not found or not active."
+        )
+    return current_user
+
+
+@router.post("/users/{user_id}/profile/", status_code=201)
+async def profile_create(
+    user_id: int,
+    profile_data: ProfileRequestSchema = Depends(
+        ProfileRequestSchema.as_form
+    ),
+    avatar: UploadFile = Depends(validate_avatar),
+    current_user: UserModel = Depends(get_current_user),
+    s3_client: S3StorageInterface = Depends(get_s3_storage_client),
+    db: AsyncSession = Depends(get_db)
+) -> ProfileResponseSchema:
+    query = select(UserModel).where(
+        UserModel.id == user_id
+    )
+    result = await db.execute(query)
+    user = result.scalar_one_or_none()
+    if not user or not user.is_active:
+        raise HTTPException(
+            status_code=401,
+            detail="User not found or not active."
+        )
+    if current_user.id != user_id and current_user.group.name != "admin":
+        raise HTTPException(
+            status_code=403,
+            detail="You don't have permission to edit this profile."
+        )
+    user_profile = await db.execute(
+        select(UserProfileModel).where(
+            UserProfileModel.user_id == user_id
+        )
+    )
+    if user_profile.scalar_one_or_none():
+        raise HTTPException(
+            status_code=400,
+            detail="User already has a profile."
+        )
+    file_name = f"avatars/{user_id}_avatar.jpg"
+    file_data = await avatar.read()
+    try:
+        await s3_client.upload_file(
+            file_name=file_name,
+            file_data=file_data
+        )
+    except Exception:
+        raise HTTPException(
+            status_code=500,
+            detail="Failed to upload avatar. Please try again later."
+        )
+    avatar_url = await s3_client.get_file_url(
+        file_name=file_name
+    )
+    profile = UserProfileModel(
+        user_id=user.id,
+        first_name=profile_data.first_name,
+        last_name=profile_data.last_name,
+        gender=profile_data.gender,
+        date_of_birth=profile_data.date_of_birth,
+        info=profile_data.info,
+        avatar=avatar_url
+    )
+    db.add(profile)
+    await db.commit()
+    await db.refresh(profile)
+    return ProfileResponseSchema.model_validate(
+        id=profile.id,
+        user_id=user.id,
+        first_name=profile.first_name,
+        last_name=profile.last_name,
+        gender=profile.gender,
+        date_of_birth=profile.date_of_birth,
+        info=profile.info,
+        avatar=profile.avatar
+    )
--- src/schemas/profiles.py
+++ src/schemas/profiles.py
@@ -1,13 +1,84 @@
 from datetime import date
-
-from fastapi import UploadFile, Form, File, HTTPException
-from pydantic import BaseModel, field_validator, HttpUrl
+from fastapi import Form
+from pydantic import BaseModel, field_validator, ConfigDict
 
 from validation import (
     validate_name,
-    validate_image,
     validate_gender,
     validate_birth_date
 )
 
-# Write your code here
+
+class ProfileRequestSchema(BaseModel):
+    first_name: str
+    last_name: str
+    gender: str
+    date_of_birth: date
+    info: str
+
+    @field_validator("first_name")
+    @classmethod
+    def valid_first_name(cls, value: str) -> str:
+        result = validate_name(
+            name=value
+        )
+        return result
+
+    @field_validator("last_name")
+    @classmethod
+    def valid_last_name(cls, value: str) -> str:
+        result = validate_name(
+            name=value
+        )
+        return result
+
+    @field_validator("gender")
+    @classmethod
+    def valid_gender(cls, value: str) -> str:
+        result = validate_gender(gender=value)
+        return result
+
+    @field_validator("date_of_birth")
+    @classmethod
+    def valid_date(cls, value: date) -> date:
+        result = validate_birth_date(
+            birth_date=value
+        )
+        return result
+
+    @field_validator("info")
+    @classmethod
+    def valid_info(cls, value: str) -> str:
+        if value is None or value is "   ":
+            raise ValueError("Info field cannot be empty or contain only spaces.")
+        return value
+
+    @classmethod
+    def as_form(
+        cls,
+        first_name: str = Form(...),
+        last_name: str = Form(...),
+        gender: str = Form(...),
+        date_of_birth: date = Form(...),
+        info: str = Form(...)
+    ):
+        return cls(
+            first_name=first_name,
+            last_name=last_name,
+            gender=gender,
+            date_of_birth=date_of_birth,
+            info=info
+        )
+
+
+class ProfileResponseSchema(BaseModel):
+    id: int
+    user_id: int
+    first_name: str
+    last_name: str
+    gender: str
+    date_of_birth: date
+    info: str
+    avatar: str
+
+    model_config = ConfigDict(from_attributes=True)
